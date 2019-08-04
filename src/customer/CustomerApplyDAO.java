9c68f0ad686b359e245114c59a0a8c9c2a267a5c
jangsusi <jsb568@naver.com>
2019-08-04 23:50:46 +0900

    그냥

---
+++ src/customer/CustomerApplyDAO.java
@@ -0,0 +1,9 @@
+package customer;
+
+import java.util.List;
+
+public interface CustomerApplyDAO {
+	public void add(CustomerApplyVO vo) throws Exception;
+	public List<CustomerApplyVO> findAll() throws Exception;
+	public void update(String helperID,String card_num,String price) throws Exception;
+}
--- src/customer/CustomerVO.java
+++ src/customer/CustomerVO.java
@@ -1,5 +1,33 @@
 package customer;
-
+/*
+ * create table customer(
+	customer_phone varchar2(11) not null, 
+	customer_addr_front varchar2(40) not null ,
+	customer_addr_detail varchar2(45) not null
+);
+ * 
+ * 
+ * */
 public class CustomerVO {
-
+	private String customer_phone;
+	public String getCustomer_phone() {
+		return customer_phone;
+	}
+	public void setCustomer_phone(String customer_phone) {
+		this.customer_phone = customer_phone;
+	}
+	public String getAddr_front() {
+		return customer_addr_front;
+	}
+	public void setAddr_front(String customer_addr_front) {
+		this.customer_addr_front = customer_addr_front;
+	}
+	private String customer_addr_front;
+	private String customer_addr_detail;
+	public String getAddr_detail() {
+		return customer_addr_detail;
+	}
+	public void setAddr_detail(String customer_addr_detail) {
+		this.customer_addr_detail = customer_addr_detail;
+	}
 }
