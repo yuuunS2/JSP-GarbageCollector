6bb71b9529ed4450e1b1a8c4e5b5de751ae9b698
jangsusi <jsb568@naver.com>
2019-08-05 00:33:55 +0900

    subin commit test

--- src/customer/CustomerApplyDAO.java
+++ src/customer/CustomerApplyDAO.java
@@ -2,6 +2,7 @@ package customer;
 
 import java.util.List;
 
+// git commit test
 public interface CustomerApplyDAO {
 	public void add(CustomerApplyVO vo) throws Exception;
 	public List<CustomerApplyVO> findAll() throws Exception;
